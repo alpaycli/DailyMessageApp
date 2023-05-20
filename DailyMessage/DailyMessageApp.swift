9bb923100e1dfa97271f0e8c5b77e6075bd650ac
alpay <calalli1903@gmail.com>
2023-05-20 15:49:26 +0400

    Initial Commit

---
+++ DailyMessage/DailyMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMessageApp.swift
+//  DailyMessage
+//
+//  Created by Alpay Calalli on 20.05.23.
+//
+
+import SwiftUI
+
+@main
+struct DailyMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
